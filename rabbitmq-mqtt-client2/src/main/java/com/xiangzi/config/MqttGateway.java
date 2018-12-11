6d5f0851577047b6fe912b165c2d303b28b946f0
hayesswing <hayesSwing@gmail.com>
2018-12-11 15:16:45 +0800

    重载消息发送方法

--- rabbitmq-mqtt-client2/src/main/java/com/xiangzi/config/MqttGateway.java
+++ rabbitmq-mqtt-client2/src/main/java/com/xiangzi/config/MqttGateway.java
@@ -7,6 +7,10 @@ import org.springframework.messaging.handler.annotation.Header;
 @MessagingGateway(defaultRequestChannel = "mqttOutboundChannel")
 public interface MqttGateway {
 
-	void sendToMqtt(String data, @Header(MqttHeaders.TOPIC) String topic);
+	void sendToMqtt(String data);
 	
+	void sendToMqtt(String data, @Header(MqttHeaders.TOPIC) String topic);
+
+    void sendToMqtt(@Header(MqttHeaders.TOPIC) String topic, @Header(MqttHeaders.QOS) int qos, String payload);
+    
 }
